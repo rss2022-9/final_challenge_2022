7951eed7739b4e87a697e3cdf388b426b3ca4379
Nisarg Dharia <nisargdharia@gmail.com>
2022-04-20 19:57:54 +0000

    Created basic templates

---
+++ final_race/src/city_driving.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+from nav_msgs.msg import Odometry
+
+class CityDriving(object):
+    """
+    Drives around a simulated city environment
+    """
+    def __init__(self):
+        self.sub_topic = rospy.get_param("~sub_topic")
+        self.pub_topic = rospy.get_param("~pub_topic")
+
+        self.sub = rospy.Subscriber(self.sub_topic, Odometry, self.callback)
+        self.pub = rospy.Publisher(self.pub_topic, Odometry, queue_size=10)
+
+    def callback(self):
+        pass
+
+if __name__=="__main__":
+    rospy.init_node("city_driving")
+    cd = CityDriving()
+    rospy.spin()
