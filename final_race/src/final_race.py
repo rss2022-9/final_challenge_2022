1ddcf72023069e1fb7c5977f72093576b6bd83b0
Zhenyang Chen <chen-zhenyang@outlook.com>
2022-04-23 13:30:02 -0400

    Simplified PP controller

--- final_race/src/final_race.py
+++ final_race/src/final_race.py
@@ -3,17 +3,23 @@
 import rospy
 import numpy as np
 from nav_msgs.msg import Odometry
+from ackermann_msgs.msg import AckermannDriveStamped
+from visual_servoing.msg import ConeLocation, ConeLocationPixel
 
 class FinalRace(object):
     """
     Completes a lap around a track.
     """
     def __init__(self):
-        self.sub_topic = rospy.get_param("~sub_topic")
-        self.pub_topic = rospy.get_param("~pub_topic")
+        self.target_topic = rospy.get_param("~target_topic")
+        self.drive_topic = rospy.get_param("~drive_topic")
 
-        self.sub = rospy.Subscriber(self.sub_topic, Odometry, self.callback)
-        self.pub = rospy.Publisher(self.pub_topic, Odometry, queue_size=10)
+        self.target_sub = rospy.Subscriber(self.target_topic, ConeLocation, self.PPController)
+        self.drive_pub = rospy.Publisher(self.drive_topic, AckermannDriveStamped, queue_size=1)
+        
+        self.lookahead        = 1.0
+        self.speed            = 1.0
+        self.wheelbase_length = 0.325
 
         # Things to do:
         # 1) Subscribe to the zed camera image
@@ -22,8 +28,26 @@ class FinalRace(object):
         # 4) Transform the goal from (u, v) to (x, y) in car frame (using homography transform?)
         # 5) Use pure pursuit to drive towards (x, y) coords
 
-    def callback(self):
-        pass
+    def PPController(self, target):
+        """
+        Target is a normal non-np 1x2 matrix of xy displacement vector from the
+        camera to the point on the ground plane.
+        Camera points along positive x axis and y axis increases to the left of
+        the camera.
+        Units are in meters.
+        """
+        x = target.x_pos
+        y = target.y_pos
+        x = -y # from camera frame to PP frame
+        tr = (self.lookahead**2)/(2*x) if x != 0 else 0 # Turning radius from relative x
+        ang = -np.arctan(self.wheelbase_length/tr) # Angle from turning radius
+        output = max(min(ang,0.34),-0.34)
+        drive_cmd = AckermannDriveStamped()
+        drive_cmd.header.stamp = rospy.Time.now()
+        drive_cmd.header.frame_id = 'base_link'
+        drive_cmd.drive.speed = self.speed
+        drive_cmd.drive.steering_angle = output
+        self.drive_pub.publish(drive_cmd)
 
 if __name__=="__main__":
     rospy.init_node("final_race")
